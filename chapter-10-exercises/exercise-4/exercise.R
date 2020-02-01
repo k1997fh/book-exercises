e400bc819420a789026671b0ecf98a4119deac7c
Rongbin Li <k1997fhok@gmail.com>
2020-01-31 18:31:09 -0800

    Submitting week 4 exercises

--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -4,36 +4,40 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
-
+grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- grants[["organization"]]
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean(grants[["total_amount"]])
+# $2600197
 
 # What was the dollar amount of the largest grant?
-
+max_grant <- max(grants[["total_amount"]])
+# $100000000
 
 # What was the dollar amount of the smallest grant?
-
+min_grant <- min(grants[["total_amount"]])
+# $5000
 
 # Which organization received the largest grant?
-
+grants[grants["total_amount"] == max_grant, "organization"]
+# "Hillsborough County Public Schools"
 
 # Which organization received the smallest grant?
-
+grants[grants["total_amount"] == min_grant, "organization"]
+#  "New Mexico Business Roundtable for Educational Excellence"
 
 # How many grants were awarded in 2010?
-
+grants_2010 <- grants[substring(grants["Grant.start.date"], 7, 10) == "2010", ]
--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -1,42 +1,50 @@
 # Exercise 4: practicing with dplyr
 
+# install.packages("dplyr")
+library("dplyr")
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
+#install.packages("nycflights13")
+library("nycflights13")
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+View(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, time_lost = (arr_delay - dep_delay))
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- arrange(flights, time_lost)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+flights <- flights %>%
+  mutate(time_lost = arr_delay - dep_delay) %>%
+  arrange(time_lost)
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights$time_lost)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+# Flights lost time on average. 
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+flights_seatac <- flights %>%
+  filter(dest == "SEA") %>%
+  pull(origin, dest, time_lost)
 
 # On average, did flights to SeaTac gain or loose time?
-
+hist(flights_seatac)
+# Lost time on average
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
